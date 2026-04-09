a090f674aef50bc98a91d8028b83825a8920ec80
Don <dhruv1227singh@gmail.com>
2026-04-09 14:55:04 +0000

    fullstack -frontend-backend-db

--- Notes/backend/index.js
+++ Notes/backend/index.js
@@ -1,4 +1,7 @@
+require('dotenv').config()
 const express = require('express')
+const Note = require('./models/note')
+
 const app = express()
 
 app.use(express.json())
@@ -38,18 +41,15 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
     response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find(note => note.id === id)
-  
-  if (note) {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
 
 
@@ -61,13 +61,6 @@ app.delete('/api/notes/:id', (request, response) => {
 })
 
 
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id)))
-    : 0
-  return String(maxId + 1)
-}
-
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
@@ -77,16 +70,14 @@ app.post('/api/notes', (request, response) => {
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
-
-  console.log(note)
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 const unknownEndPoint = (request, response) => {
@@ -96,6 +87,6 @@ const unknownEndPoint = (request, response) => {
 app.use(unknownEndPoint)
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
--- Notes/backend/mongo.js
+++ Notes/backend/mongo.js
@@ -7,7 +7,7 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://fullstack:${password}@cluster0.a5qfl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const url = `mongodb+srv://don_db_user:${password}@cluster0.r1mxiff.mongodb.net/notesApp?appName=Cluster0`
 
 mongoose.set('strictQuery',false)
 
@@ -21,7 +21,7 @@ const noteSchema = new mongoose.Schema({
 const Note = mongoose.model('Note', noteSchema)
 
 const note = new Note({
-  content: 'HTML is easy',
+  content: 'CSS is tricky',
   important: true,
 })
 
