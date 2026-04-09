390d96d56736b8af26cdf408491e9dd27a214552
Don <dhruv1227singh@gmail.com>
2026-04-09 15:25:30 +0000

    uibuild

--- Notes/backend/index.js
+++ Notes/backend/index.js
@@ -18,24 +18,6 @@ const requestLogger = (request, response, next) => {
 
 app.use(requestLogger)
 
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
-
 app.get('/', (request, response) => {
     response.send('<h1>Hello World</h1>')
 })
